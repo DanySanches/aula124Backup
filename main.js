e0ffbe9f1b9f35b59bfce1ea71749c85db8a24ce
danysanches@msn.com <danysanches@msn.com>
2023-09-20 17:39:54 -0300

    backup

---
+++ main.js
@@ -0,0 +1,25 @@
+noseX=0;
+noseY=0;
+
+// carregaremos o  nariz nas próximas aulas
+function preload() {
+ 
+}
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  // função p5.js predefinida usada para colocar a tela no centro da página.
+  canvas.center();
+
+
+
+}
+
+function draw() {
+
+}
+
+function tirarFoto(){    
+  save('minhaFoto.png');
+}
+
